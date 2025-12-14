bebe41bf73bbc98481c75b8a8d32b837c0879930
Eric Laquer <LaquerEric@gmail.com>
2025-12-14 10:53:58 -0500

    Add rails8-json-rpc-demo as proper submoduler managed submodule
    - Initialized git repository in rails8-json-rpc-demo directory
    - Created GitHub repository: magenticmarketactualskill/rails8-json-rpc-demo
    - Pushed initial commit with all project files
    - Added as git submodule with proper .gitmodules entry
    - Added submodule configuration to .submoduler.ini
    - Now properly managed by submoduler system

--- rails8-json-rpc-demo/client_app/test/models/outgoing_record_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class OutgoingRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- rails8-json-rpc-demo/server_app/test/models/received_record_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ReceivedRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
